67be3292549b821baf40c1af81f1030bc2834e9d
Trust Jonathan <trustjonathan256@gmail.com>
2026-08-06 17:39:17 +0300

    23432134

--- app/admin/nfts/[id]/page.tsx
+++ app/admin/nfts/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "../../../utils/supabase/server";
 import { notFound } from "next/navigation";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import EditAppForm from "./EditAppForm";
 
@@ -33,7 +33,7 @@ export default async function EditNFTPage({
           href="/admin/nfts"
           className="p-2 bg-white/5 rounded-full hover:bg-white/10 transition-colors"
         >
-          <ArrowLeft className="w-5 h-5 text-white" />
+          <ArrowLeftIcon className="w-5 h-5 text-white" />
         </Link>
         <h2 className="text-3xl font-bold text-white">Edit App</h2>
       </div>
